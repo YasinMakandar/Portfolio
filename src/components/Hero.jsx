0cd271a1429b845774ca2bcf781201f88c72836b
YasinMakandar <yasinmakandar03@gmail.com>
2024-09-15 04:51:18 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -686,7 +686,7 @@ const Hero = () => {
                             </div>
                         </div>
                         <div className="mt-10 text-xs">
-                            <p>Made with ❤️ by Yasin Makandar</p>
+                            <p>Made with ⌨️&🖱️ by Yasin Makandar</p>
                         </div>
                     </div>
                 </div>
